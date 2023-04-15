b70b17a10811d323c49a7451ec6891897f3807aa
feb27 <feb27@10.0.0.123>
2023-04-15 09:07:55 -0400

    IMPLEMENTED multiply()

--- src/hw2Project4/DemoProject4.java
+++ src/hw2Project4/DemoProject4.java
@@ -2,12 +2,17 @@ package hw2Project4;
 
 public class DemoProject4 {
 
-	public static void main (String args []) {
-		
+	public static void main(String args[]) {
+
 		System.out.println("Demo 4 Pushing to Remote Repository...");
 	}
-	
+
 	public void subtract(int a, int b) {
-		System.out.println(a-b);
-	
-}}
+		System.out.println(a - b);
+
+	}
+
+	public void multiply(int a, int b) {
+		System.out.println(a*b);
+	}
+}
