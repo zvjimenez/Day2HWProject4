20b8d06d0c7b1a9d1548db1e2fb8a0e6a4b02917
feb27 <feb27@10.0.0.123>
2023-04-15 09:00:21 -0400

    INITIAL commit

---
+++ src/hw2Project4/DemoProject4.java
@@ -0,0 +1,13 @@
+package hw2Project4;
+
+public class DemoProject4 {
+
+	public static void main (String args []) {
+		
+		System.out.println("Demo 4 Pushing to Remote Repository...");
+	}
+	
+	public void subtract(int a, int b) {
+		System.out.println(a-b);
+	
+}}
